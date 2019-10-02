e057d9377425586e5ffc13a37431a55814f0cdf4
Jesús Marcos Gálvez <55401557+Jesus-DAW@users.noreply.github.com>
2019-10-02 08:24:28 +0200

    Add files via upload

---
+++ Tema 2 Programación/ejer3.java
@@ -0,0 +1,17 @@
+/*
+*
+* Ejercicio 3: Mostrar datos personales
+*
+*/
+
+public class ejer3 {
+	public static void main(String[] args) {
+		String nombre = "Jesús";
+		String dir = "C/Marbellíes 47";
+		String tel = "123456789";
+		
+		System.out.println(nombre);
+		System.out.println(dir);
+		System.out.println(tel);
+	}
+}
